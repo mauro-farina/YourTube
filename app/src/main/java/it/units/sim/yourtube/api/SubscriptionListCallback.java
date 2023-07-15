0b3929cb75777b88adfc86f0b754ad9c1a845a4a
Mauro Farina <maurofarina@protonmail.com>
2023-07-15 18:03:55 +0200

    remove SubscriptionListCallback

--- app/src/main/java/it/units/sim/yourtube/api/SubscriptionListCallback.java
+++
@@ -1,8 +0,0 @@
-package it.units.sim.yourtube.api;
-
-import java.util.List;
-
-import it.units.sim.yourtube.model.UserSubscription;
-
-public interface SubscriptionListCallback extends RequestCallback<List<UserSubscription>> {
-}
