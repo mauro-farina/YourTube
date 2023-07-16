42ba1b828baa25be2e0bd7d2406e38c005650a42
Mauro Farina <maurofarina@protonmail.com>
2023-07-16 13:27:36 +0200

    add logout functionality

--- app/src/main/java/it/units/sim/yourtube/MainActivity.java
+++ app/src/main/java/it/units/sim/yourtube/MainActivity.java
@@ -4,8 +4,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -21,9 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         GoogleAccountCredential credential = GoogleCredentialManager.getInstance().getCredential();
         if (credential.getSelectedAccountName() == null) {
-            finish();
-            Intent intent = new Intent(this, AuthenticationActivity.class);
-            startActivity(intent);
+            openAuthenticationActivity();
         }
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
@@ -38,6 +39,32 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.top_bar_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logoutButton) {
+            logout();
+            return true;
+        } else if (id == R.id.settingsButton) {
+            // Transaction to Settings fragment
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.remove("accountName");
+        editor.apply();
+        openAuthenticationActivity();
+    }
+
+    private void openAuthenticationActivity() {
+        finish();
+        Intent intent = new Intent(this, AuthenticationActivity.class);
+        startActivity(intent);
     }
 
 }
