d819910e4455e0258968e58aab5679eebb85ca3f
Mauro Farina <maurofarina@protonmail.com>
2023-07-18 17:45:36 +0200

    refactor

--- app/src/main/java/it/units/sim/yourtube/VideosFragment.java
+++ app/src/main/java/it/units/sim/yourtube/VideosFragment.java
@@ -3,6 +3,7 @@ package it.units.sim.yourtube;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,40 +12,17 @@ import android.view.ViewGroup;
 public class VideosFragment extends Fragment {
 
     public VideosFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Homepage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VideosFragment newInstance(String param1, String param2) {
-        VideosFragment fragment = new VideosFragment();
-        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        super(R.layout.fragment_videos);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_videos, container, false);
     }
 }
