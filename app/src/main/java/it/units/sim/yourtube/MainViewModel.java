2cde90e910aa71d348b32001f9ccfa1bc1e5d990
Mauro Farina <maurofarina@protonmail.com>
2023-07-19 21:19:31 +0200

    add fetchVideos

--- app/src/main/java/it/units/sim/yourtube/MainViewModel.java
+++ app/src/main/java/it/units/sim/yourtube/MainViewModel.java
@@ -7,17 +7,23 @@ import androidx.lifecycle.ViewModel;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 import it.units.sim.yourtube.api.RequestCallback;
 import it.units.sim.yourtube.api.RequestThread;
 import it.units.sim.yourtube.api.SubscriptionListRequest;
+import it.units.sim.yourtube.api.VideoUploadsRequest;
 import it.units.sim.yourtube.model.UserSubscription;
+import it.units.sim.yourtube.model.VideoData;
 
 public class MainViewModel extends ViewModel {
 
     private final MutableLiveData<List<UserSubscription>>
             subscriptionsList = new MutableLiveData<>(new ArrayList<>());
+    private final MutableLiveData<List<VideoData>>
+            videosList = new MutableLiveData<>(new ArrayList<>());
 
     public void fetchUserSubscriptions() {
         GoogleAccountCredential credential = GoogleCredentialManager.getInstance().getCredential();
@@ -30,10 +36,28 @@ public class MainViewModel extends ViewModel {
     }
 
     public void fetchVideos() {
+        GoogleAccountCredential credential = GoogleCredentialManager.getInstance().getCredential();
+        for (UserSubscription sub : Objects.requireNonNull(subscriptionsList.getValue())) {
+            VideoUploadsRequest subscriptionRequest = new VideoUploadsRequest(credential, sub, new Date());
+            RequestCallback<List<VideoData>> subscriptionListCallback = list -> {
+                List<VideoData> videos = videosList.getValue();
+                if (videos != null) {
+                    videos.addAll(list);
+                    videosList.setValue(videos);
+                }
+            };
+            RequestThread<List<VideoData>> rThread =
+                    new RequestThread<>(subscriptionRequest, subscriptionListCallback);
+            rThread.start();
+        }
     }
 
     public LiveData<List<UserSubscription>> getSubscriptionsList() {
         return subscriptionsList;
+    }
+
+    public LiveData<List<VideoData>> getVideosList() {
+        return videosList;
     }
 
 }
