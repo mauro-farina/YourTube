fcd5af4126c16ff761fe8e210d5baf59bbfaa3c3
Mauro Farina <maurofarina@protonmail.com>
2023-07-26 14:16:17 +0200

    create categories fragment

--- app/src/main/java/it/units/sim/yourtube/CategoriesFragment.java
+++ app/src/main/java/it/units/sim/yourtube/CategoriesFragment.java
@@ -3,13 +3,24 @@ package it.units.sim.yourtube;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+import it.units.sim.yourtube.model.Category;
+
 public class CategoriesFragment extends Fragment {
+
+    private CategoriesAdapter adapter;
+    private List<Category> categories;
+
     public CategoriesFragment() {
-        // Required empty public constructor
+        super(R.layout.fragment_categories);
     }
 
     @Override
@@ -18,9 +29,15 @@ public class CategoriesFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false);
+        View view = inflater.inflate(R.layout.fragment_categories, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.categories_recycler_view);
+        categories = new ArrayList<>();
+        adapter = new CategoriesAdapter(categories);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
     }
 }
