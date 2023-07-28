919d3ee19018c1c3c72fa40156055de8c943c3d9
Mauro Farina <maurofarina@protonmail.com>
2023-07-28 12:43:31 +0200

    add subsList to new category dialog

--- app/src/main/java/it/units/sim/yourtube/CategoriesFragment.java
+++ app/src/main/java/it/units/sim/yourtube/CategoriesFragment.java
@@ -1,5 +1,6 @@
 package it.units.sim.yourtube;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,19 +12,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import it.units.sim.yourtube.model.Category;
+import it.units.sim.yourtube.model.UserSubscription;
 
 public class CategoriesFragment extends Fragment {
 
     private CategoriesAdapter adapter;
     private CategoriesViewModel categoriesViewModel;
+    private MainViewModel subscriptionsViewModel;
 
     public CategoriesFragment() {
         super(R.layout.fragment_categories);
@@ -32,6 +39,7 @@ public class CategoriesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        subscriptionsViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
         categoriesViewModel = new ViewModelProvider(requireActivity()).get(CategoriesViewModel.class);
         categoriesViewModel.fetchCategories();
         adapter = new CategoriesAdapter(new ArrayList<>());
@@ -51,18 +59,57 @@ public class CategoriesFragment extends Fragment {
         FloatingActionButton fab = view.findViewById(R.id.categories_add_category_fab);
         fab.setOnClickListener(v -> {
             // dialog
-            View dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.add_category_dialog, null);
+            View dialogView = LayoutInflater
+                    .from(requireContext())
+                    .inflate(R.layout.add_category_dialog, null);
+            // adapter for recyclerview
+            RecyclerView subscriptionsRecyclerView =
+                    dialogView.findViewById(R.id.new_category_dialog_channels_list);
+            List<String> selectedChannels = new ArrayList<>();
+            SubscriptionsAdapter subscriptionsAdapter = new SubscriptionsAdapter(
+                    subscriptionsViewModel.getSubscriptionsList().getValue(),
+                    clickedView -> {
+                        TextView selectedChannelTextView = clickedView.findViewById(R.id.list_item_subscription_channel_name);
+                        String selectedChannelName = selectedChannelTextView.getText().toString();
+                        UserSubscription selectedChannel = Objects
+                                .requireNonNull(
+                                    subscriptionsViewModel
+                                    .getSubscriptionsList()
+                                    .getValue()
+                                )
+                                .stream()
+                                .filter(sub -> sub.getChannelName().equals(selectedChannelName))
+                                .findFirst()
+                                .orElse(null);
+                        if (selectedChannel == null) {
+                            System.err.println("Something very wrong just happened...");
+                            return;
+                        }
+                        selectedChannelTextView.setTypeface(selectedChannelTextView.getTypeface(), Typeface.BOLD);
+                        selectedChannels.add(selectedChannel.getChannelId());
+                    }
+            );
+
+            subscriptionsRecyclerView.setAdapter(subscriptionsAdapter);
+            subscriptionsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
             EditText input = dialogView.findViewById(R.id.new_category_dialog_name);
+
             new MaterialAlertDialogBuilder(requireContext())
                     .setTitle(R.string.new_category_dialog_title)
                     .setView(dialogView)
-                    .setPositiveButton("ADD", (dialog, which) -> addCategory(input.getText().toString()))
+                    .setPositiveButton("ADD", (dialog, which) -> {
+                        System.out.println(selectedChannels);
+                        addCategory(input.getText().toString());
+                    })
                     .show();
         });
         return view;
     }
 
     private void addCategory(String name) {
+        name = name.trim();
+        if (name.length() == 0)
+            return;
         Toast.makeText(requireContext(), name, Toast.LENGTH_SHORT).show();
         Category newCategory = new Category(name);
         categoriesViewModel.addCategory(newCategory);
