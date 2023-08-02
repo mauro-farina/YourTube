1d04d9c6e0ac8ccd4cf7858ce6f83400fbb08786
Mauro Farina <maurofarina@protonmail.com>
2023-08-02 16:17:45 +0200

    remove unused imports

--- app/src/main/java/it/units/sim/yourtube/DialogCategoryOptions.java
+++ app/src/main/java/it/units/sim/yourtube/DialogCategoryOptions.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
