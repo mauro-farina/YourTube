df9505b1a3f4ae56ce185c5b32724b28e9669f2a
Mauro Farina <maurofarina@protonmail.com>
2023-08-12 19:44:50 +0200

    remove comment

--- app/src/main/java/it/units/sim/yourtube/MainActivity.java
+++ app/src/main/java/it/units/sim/yourtube/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.top_app_bar);
         setSupportActionBar(toolbar);
 
-//        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         ViewModelProvider.AndroidViewModelFactory factory =
                 ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication());
         MainViewModel viewModel = new ViewModelProvider(this, factory).get(MainViewModel.class);
