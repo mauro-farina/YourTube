abceddc84ca9c0faa9aaa186a5710c8b00c695bb
Mauro Farina <maurofarina@protonmail.com>
2023-08-17 13:21:18 +0200

    add fix for toast on

--- app/src/main/java/it/units/sim/yourtube/NetworkChangeReceiver.java
+++ app/src/main/java/it/units/sim/yourtube/NetworkChangeReceiver.java
@@ -9,10 +9,17 @@ import android.widget.Toast;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
 
+    private static boolean first = true;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+
+        if (first) {
+            first = false;
+            return;
+        }
 
         if (activeNetwork != null && activeNetwork.isConnected()) {
             Toast.makeText(context, "Internet connection is back", Toast.LENGTH_SHORT).show();
