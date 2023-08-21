3ad0598f95ef0161999329725b0dfe1303f430ba
Mauro Farina <maurofarina@protonmail.com>
2023-08-21 16:40:57 +0200

    add setLanguage manager

--- app/src/main/java/it/units/sim/yourtube/SettingsManager.java
+++ app/src/main/java/it/units/sim/yourtube/SettingsManager.java
@@ -1,11 +1,18 @@
 package it.units.sim.yourtube;
 
+import android.content.Context;
+import android.content.res.Configuration;
+
 import androidx.appcompat.app.AppCompatDelegate;
+
+import java.util.Locale;
 
 public class SettingsManager {
 
     public static final String PREFERENCE_THEME_DEFAULT = "system";
+    public static final String PREFERENCE_LANGUAGE_DEFAULT = "system";
     public static final String PREFERENCE_THEME = "theme_preference";
+    public static final String PREFERENCE_LANGUAGE = "language_preference";
 
     public static void setTheme(String newTheme) {
         switch (newTheme) {
@@ -18,6 +25,14 @@ public class SettingsManager {
             default:
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
         }
+    }
+
+    public static void setLanguage(Context context, String newLanguage) {
+        Locale locale = newLanguage.equals(PREFERENCE_LANGUAGE_DEFAULT) ?
+                Locale.getDefault() : new Locale(newLanguage);
+        Configuration configuration = context.getResources().getConfiguration();
+        configuration.setLocale(locale);
+        context.getResources().updateConfiguration(configuration, context.getResources().getDisplayMetrics());
     }
 
 }
