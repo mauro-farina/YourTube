768325d16aa3dc7e18193bcd0a0fafd09c4e21b3
Mauro Farina <maurofarina@protonmail.com>
2023-09-12 16:02:07 +0200

    add new categories to enum class

--- app/src/main/java/it/units/sim/yourtube/model/CategoryIcon.java
+++ app/src/main/java/it/units/sim/yourtube/model/CategoryIcon.java
@@ -11,6 +11,12 @@ public enum CategoryIcon implements Serializable {
     ICON_CATEGORY_TRAVEL(R.drawable.icon_category_travel),
     ICON_CATEGORY_VIDEOGAME(R.drawable.icon_category_videogame),
     ICON_CATEGORY_COMPUTER(R.drawable.icon_category_computer),
+    ICON_CATEGORY_PET(R.drawable.icon_category_pet),
+    ICON_CATEGORY_FINANCE(R.drawable.icon_category_finance),
+    ICON_CATEGORY_FOOD(R.drawable.icon_category_food),
+    ICON_CATEGORY_HEALTH(R.drawable.icon_category_health),
+    ICON_CATEGORY_SCHOOL(R.drawable.icon_category_school),
+    ICON_CATEGORY_SHOPPING(R.drawable.icon_category_shopping),
     ICON_CATEGORY_PODCAST(R.drawable.icon_category_podcast);
 
     private final int resourceId;
