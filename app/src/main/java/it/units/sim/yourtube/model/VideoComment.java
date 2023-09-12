114d405c7cb75e03945d53031b1c5eeb7afc8e31
Mauro Farina <maurofarina@protonmail.com>
2023-09-12 11:59:50 +0200

    remove unused fields

--- app/src/main/java/it/units/sim/yourtube/model/VideoComment.java
+++ app/src/main/java/it/units/sim/yourtube/model/VideoComment.java
@@ -1,32 +1,19 @@
 package it.units.sim.yourtube.model;
 
-import com.google.api.client.util.DateTime;
 import com.google.api.services.youtube.model.CommentSnippet;
 
 public class VideoComment {
 
-    private final long likesCount;
     private final String author;
-    private final DateTime publishedAt;
     private final String comment;
 
     public VideoComment(CommentSnippet commentSnippet) {
-        this.likesCount = commentSnippet.getLikeCount();
         this.author = commentSnippet.getAuthorDisplayName();
-        this.publishedAt = commentSnippet.getPublishedAt();
         this.comment = commentSnippet.getTextOriginal();
-    }
-
-    public long getLikesCount() {
-        return likesCount;
     }
 
     public String getAuthor() {
         return author;
-    }
-
-    public DateTime getPublishedAt() {
-        return publishedAt;
     }
 
     public String getComment() {
