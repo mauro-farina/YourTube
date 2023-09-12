ae454b7b52d7791fe1fa2854c12815ae6a99e30e
Mauro Farina <maurofarina@protonmail.com>
2023-09-12 12:55:40 +0200

    add CategoryIcon model class

---
+++ app/src/main/java/it/units/sim/yourtube/model/CategoryIcon.java
@@ -0,0 +1,24 @@
+package it.units.sim.yourtube.model;
+
+import java.io.Serializable;
+
+import it.units.sim.yourtube.R;
+
+public enum CategoryIcon implements Serializable {
+    ICON_CATEGORY_BASKETBALL(R.drawable.icon_category_basketball),
+    ICON_CATEGORY_CAR(R.drawable.icon_category_car),
+    ICON_CATEGORY_TRAVEL(R.drawable.icon_category_travel),
+    ICON_CATEGORY_VIDEOGAME(R.drawable.icon_category_videogame),
+    ICON_CATEGORY_COMPUTER(R.drawable.icon_category_computer),
+    ICON_CATEGORY_PODCAST(R.drawable.icon_category_podcast);
+
+    private final int resourceId;
+
+    CategoryIcon(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+}
